a6ce8d4731de073e691d79126af306b147fe667b
ara-tinyerp <ara@tinyerp.com>
2014-02-11 15:44:41 +0530

    [IMP] improve account invoice report

--- sparetech_report/report/account_print_invoice.py
+++ sparetech_report/report/account_print_invoice.py
@@ -19,7 +19,19 @@
 #
 ##############################################################################
 
-import sale_order
-import request_quotation
-import account_print_invoice
+import time
+from openerp.report import report_sxw
+
+class account_invoice(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(account_invoice, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+report_sxw.report_sxw(
+    'report.sparetech.account.invoice',
+    'account.invoice',
+    'sparetech_report/report/account_print_invoice.rml',
+    parser=account_invoice,header=False
+)
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
