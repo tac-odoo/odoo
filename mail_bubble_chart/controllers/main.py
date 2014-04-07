ba8b2efa8b3bbb3e9696cca2b1bbb7f5408d9204
Mohammed Shekha <msh@tinyerp.com>
2014-04-07 15:02:29 +0530

    [FIX]Fixed the issue of traceback due to import of http in controller, as module has been developed in trunk and just simply ported to trunk_india branch, removed controller imports as there is no need of controller at all now.

--- mail_bubble_chart/controllers/main.py
+++ mail_bubble_chart/controllers/main.py
@@ -1,9 +1,1 @@
-import base64
-import psycopg2
-
-import openerp
-from openerp import SUPERUSER_ID
-from openerp import http
-from openerp.http import request
-from openerp.addons.web.controllers.main import content_disposition
 
