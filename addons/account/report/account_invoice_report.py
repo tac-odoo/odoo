50a776d36f19cf8b548b30981ca0cf513a71ca7e
ssh-odoo <ssh@openerp.com>
2014-05-23 12:30:19 +0530

    [rem]:remove import time

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -19,7 +19,6 @@
 #
 ##############################################################################
 
-import time
 from openerp import tools
 import openerp.addons.decimal_precision as dp
 from openerp.osv import fields,osv
