bc5db374bd130061e6aa88c21a4bc8d73fdc60e9
Wisniewski <nwi@openerp.com>
2014-05-28 17:15:44 +0200

    [ADD] Add my_theme module

---
+++ my_theme/my_theme/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'My Theme',
+    'category': 'Website',
+    'summary': 'Customisations',
+    'version': '1.0',
+    'description': """
+Feuerwear Customisations
+======================================
+        """,
+    'author': 'fp@odoo.com',
+    'depends': ['payment','crm','stock','website_sale'],
+    'data': [
+    ],
+    'installable': True,
+}
