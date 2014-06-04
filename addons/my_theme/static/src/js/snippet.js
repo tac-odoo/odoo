b55907879214e502683c8fc1587ec905eab46432
Wisniewski <nwi@openerp.com>
2014-06-04 11:20:43 +0200

    [FIX]

--- addons/my_theme/static/src/js/snippet.js
+++ addons/my_theme/static/src/js/snippet.js
@@ -5,7 +5,10 @@
   website.snippet.animationRegistry.my_snippet = website.snippet.Animation.extend({
     selector : ".mt_simple_snippet",
     start: function(){
-      alert("Some Javascript Logic Here");
+      var h1 = this.$el.find("h1");
+      var h1_width = h1.width();
+      h1.css('width',0);
+      h1.animate( { width: h1_width }, 3000 );
     },
   });
 })();
