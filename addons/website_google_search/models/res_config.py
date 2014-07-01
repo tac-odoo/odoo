dce14c7953867944be61059177ff380563e18bbe
FalcoBolger <vta@openerp.com>
2014-07-01 12:13:30 +0200

    [IMP] Add cx to settings

--- addons/website_google_search/models/res_config.py
+++ addons/website_google_search/models/res_config.py
@@ -5,5 +5,6 @@ class website_config_settings(osv.osv_memory):
     _inherit = "website.config.settings"
 
     _columns = {
-        'google_search_published': fields.related('website_id', 'google_search_published', type='boolean', string='Show Google\'s Custom Search on Website')
+        'google_search_published': fields.related('website_id', 'google_search_published', type='boolean', string='Show Google\'s Custom Search on Website'),
+        'google_search_cx': fields.related('website_id', 'google_search_cx', type='char', string='Google\'s Custom Search Engine ID')
     }
--- addons/website_google_search/models/website.py
+++ addons/website_google_search/models/website.py
@@ -5,5 +5,6 @@ class website(osv.osv):
     _inherit = "website"
 
     _columns = {
-        'google_search_published': fields.boolean('Show Google\'s Custom Search on Website')
+        'google_search_published': fields.boolean('Show Google\'s Custom Search on Website'),
+        'google_search_cx': fields.char('Google\'s Custom Search Engine ID'),
     }
