3538652eec6abfa601951361267b39e9656b417a
Jitendra Prajapati <jpr@openerp.com>
2014-07-18 18:03:13 +0530

    [IMP]hr_evaluation: updated fields and views. set fields data on onchange
    [IMP]hr_evaluation: remove unused import

--- addons/hr_evaluation/hr_evaluation.py
+++ addons/hr_evaluation/hr_evaluation.py
@@ -25,7 +25,6 @@ from dateutil import parser
 import time
 
 from openerp import fields, api, tools, models
-#from openerp.osv import osv, orm
 from openerp.tools.translate import _
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DF
 
@@ -47,18 +46,18 @@ class hr_evaluation(models.Model):
             ('done', 'Done'),
     ], 'Status', required=True, readonly=True, copy=False, default='new')
     date_close = fields.Date('Appraisal Deadline', select=True)
-    appraisal_manager = fields.Boolean(string='Manger', store=True, related='employee_id.appraisal_manager', readonly=True)
-    apprasial_manager_ids = fields.Many2many('hr.employee', related='employee_id.apprasial_manager_ids', readonly=True)
-    apprasial_manager_survey_id = fields.Many2one('survey.survey', store=True, related='employee_id.apprasial_manager_survey_id', readonly=True)
-    appraisal_colleagues = fields.Boolean( string='Colleagues', store=True, related='employee_id.appraisal_colleagues', readonly=True)
-    appraisal_colleagues_ids = fields.Many2many('hr.employee', related='employee_id.appraisal_colleagues_ids', readonly=True)
-    appraisal_colleagues_survey_id = fields.Many2one('survey.survey', store=True, related='employee_id.appraisal_colleagues_survey_id', readonly=True)
-    appraisal_self = fields.Boolean(string='Employee', store=True, related='employee_id.appraisal_self', readonly=True)
-    apprasial_employee_id = fields.Many2one('hr.employee', store=True, related='employee_id.apprasial_employee_id', readonly=True)
-    appraisal_self_survey_id = fields.Many2one('survey.survey', store=True, related='employee_id.appraisal_self_survey_id', string='self Appraisal', readonly=True)
-    appraisal_subordinates = fields.Boolean(string='Collaborator', store=True, related='employee_id.appraisal_subordinates', readonly=True)
-    appraisal_subordinates_ids = fields.Many2many('hr.employee', related='employee_id.appraisal_subordinates_ids', readonly=True)
-    appraisal_subordinates_survey_id = fields.Many2one('survey.survey', store=True, related='employee_id.appraisal_subordinates_survey_id', readonly=True)
+    appraisal_manager = fields.Boolean('Manger',)
+    apprasial_manager_ids = fields.Many2many('hr.employee', 'evaluation_apprasial_manager_rel', 'hr_evaluation_evaluation_id')
+    apprasial_manager_survey_id = fields.Many2one('survey.survey',)
+    appraisal_colleagues = fields.Boolean('Colleagues')
+    appraisal_colleagues_ids = fields.Many2many('hr.employee', 'evaluation_appraisal_colleagues_rel', 'hr_evaluation_evaluation_id')
+    appraisal_colleagues_survey_id = fields.Many2one('survey.survey')
+    appraisal_self = fields.Boolean('Employee')
+    apprasial_employee_id = fields.Many2one('hr.employee')
+    appraisal_self_survey_id = fields.Many2one('survey.survey',string='self Appraisal',)
+    appraisal_subordinates = fields.Boolean('Collaborator')
+    appraisal_subordinates_ids = fields.Many2many('hr.employee', 'evaluation_appraisal_subordinates_rel', 'hr_evaluation_evaluation_id')
+    appraisal_subordinates_survey_id = fields.Many2one('survey.survey',)
     color = fields.Integer('Color Index')
     display_name = fields.Char(compute='_set_display_name')
 
@@ -71,16 +70,19 @@ class hr_evaluation(models.Model):
     @api.one
     @api.onchange('employee_id')
     def onchange_employee_id(self):
-        self.apprasial_employee_id = self.employee_id
         self.department_id = self.employee_id.department_id
-
-    def _read_group_stage_ids(self, cr, uid, ids, domain, read_group_order=None, access_rights_uid=None, context=None):
-        stage_obj = self.pool.get('hr_evaluation.stage')
-        stage_ids = stage_obj.search(cr, uid, [], context=context)
-        result = stage_obj.name_get(cr, uid, stage_ids, context=context)
-        return result, {}
-
-    _group_by_full = {'stage_id': _read_group_stage_ids,}
+        self.appraisal_manager = self.employee_id.appraisal_manager
+        self.apprasial_manager_ids = self.employee_id.apprasial_manager_ids
+        self.apprasial_manager_survey_id = self.employee_id.apprasial_manager_survey_id
+        self.appraisal_colleagues = self.employee_id.appraisal_colleagues
+        self.appraisal_colleagues_ids = self.employee_id.appraisal_colleagues_ids
+        self.appraisal_colleagues_survey_id = self.employee_id.appraisal_colleagues_survey_id
+        self.appraisal_self = self.employee_id.appraisal_self
+        self.apprasial_employee_id = self.employee_id
+        self.appraisal_self_survey_id = self.employee_id.appraisal_self_survey_id
+        self.appraisal_subordinates = self.employee_id.appraisal_subordinates
+        self.appraisal_subordinates_ids = self.employee_id.appraisal_subordinates_ids
+        self.appraisal_subordinates_survey_id = self.employee_id.appraisal_subordinates_survey_id
 
 class hr_employee(models.Model):
     _name = "hr.employee"
@@ -88,23 +90,10 @@ class hr_employee(models.Model):
 
     evaluation_date = fields.Date('Next Appraisal Date', help="The date of the next appraisal is computed by the appraisal plan's dates (first appraisal + periodicity).")
     appraisal_manager = fields.Boolean('Manager', help="",)
-    apprasial_manager_ids = fields.Many2many('hr.employee', 'apprasial_manager_rel', 'hr_evaluation_evaluation_id', compute='_appraisal_manager')
-
-    @api.one
-    @api.depends('parent_id','coach_id')
-    def _appraisal_manager(self):
-        self.apprasial_manager_ids = [self.parent_id.id , self.coach_id.id]
-
+    apprasial_manager_ids = fields.Many2many('hr.employee', 'apprasial_manager_rel', 'hr_evaluation_evaluation_id',)
     apprasial_manager_survey_id = fields.Many2one('survey.survey', 'Manager Appraisal',)
     appraisal_colleagues = fields.Boolean('Colleagues', help="")
-    appraisal_colleagues_ids = fields.Many2many('hr.employee', 'appraisal_colleagues_rel', 'hr_evaluation_evaluation_id', compute='_appraisal_colleagues')
-
-    @api.one
-    def _appraisal_colleagues(self):
-        colleagues = self.search([('parent_id', '=', self.parent_id.id),('coach_id', '=', self.coach_id.id)])
-        if colleagues:
-            self.appraisal_colleagues_ids = [rec.id for rec in colleagues]
-
+    appraisal_colleagues_ids = fields.Many2many('hr.employee', 'appraisal_colleagues_rel', 'hr_evaluation_evaluation_id',)
     appraisal_colleagues_survey_id = fields.Many2one('survey.survey', "Employee's Appraisal", )
     appraisal_self = fields.Boolean('Employee', help="")
     apprasial_employee_id = fields.Many2one('hr.employee', 'Employee', readonly=True)
