9ba41c343a0e280c96d7c4869691a340b4e90031
mak <mak@openerp.com>
2014-10-06 15:54:11 +0530

    Fine tune the New twitter wall model box.

--- addons/website_twitter_wall/static/src/js/website_twitter_wall.editor.js
+++ addons/website_twitter_wall/static/src/js/website_twitter_wall.editor.js
@@ -14,7 +14,12 @@
         events: {
             'click #save': 'save',
             'change #image_upload':'image_upload',
+            'click #image_upload':'file_focus',
+            'focusin #image_upload':'file_focus',
             'change #image_url':'image_url',
+            'focusin #image_url':'image_focus',
+            'change .text-wallname':'text_wallname',
+            'change .text-description':'text_description',
         },
         start: function () {
             var self = this;
@@ -23,6 +28,14 @@
             $modal.modal();
             $("#h_ele").val("");
             $('#image').attr('src','/website/image/ir.attachment/81/datas');
+        },
+        file_focus:function(e){
+            $('.image_url_body').removeClass('bg-primary');
+            $('.upload_body').addClass('bg-primary');
+        },
+        image_focus:function(e){
+            $('.upload_body').removeClass('bg-primary');
+            $('.image_url_body').addClass('bg-primary');
         },
         image_upload: function(e){
             $("#h_ele").val("");
@@ -39,17 +52,42 @@
             fr.readAsDataURL(fileName);
         },
         image_url:function(e){
+            var self = this;
             $("#h_ele").val("");
             $("#image_upload").val("");
+            self.error("");
+            $("div.error-dialog").remove();
             var url = e.target.value;
             if (url.contains(".jpg") || url.contains(".png") || url.contains(".jpeg")){
                 $('.show_image img').remove();
                 $('.show_image').html("<img src='"+url+"' id='image' class='img-responsive img-thumbnail' style='width: 100%;'/>");
                 $("#h_ele").val(url);                 
             }else{
+                self.error("");
+                self.error("Enter valid image URL.");
+                e.target.value = "";
                 return;
             }
             
+        },
+        text_wallname:function(e){
+            var self = this;
+            if (e.target.value.length > 0){
+                self.error("");
+                $("div.error-dialog").remove();    
+            }else{
+                return false;
+            }
+            
+        },
+        text_description:function(e){
+            var self = this;
+            if (e.target.value.length > 0){
+                self.error("");
+                $("div.error-dialog").remove();    
+            }else{
+                return false;
+            }
         },
         save: function () {
             var self = this;
@@ -88,7 +126,7 @@
             });
         },
         error: function (msg) {
-            $(".error_msg").html("<div class='alert alert-danger alert-dismissible' role='alert'>"+ msg +"</div>");
+            $(".error_msg").html("<div class='error-dialog alert alert-danger alert-dismissible' role='alert'>"+ msg +"</div>");
         },
     });
 })();
