8ab7e5c50329f461e3736e2a390881697f1e5ad3
jfo <jfo@odoo.com>
2014-10-10 15:57:16 +0200

    Added display of little buttons (hove ron ponderation) to up and down the ponderation

--- addons/evaluation_matrix/static/src/js/evaluation_matrix.js
+++ addons/evaluation_matrix/static/src/js/evaluation_matrix.js
@@ -5,28 +5,28 @@ $(document).ready(function () {
     website = openerp.website;
     website.add_template_file('/evaluation_matrix/static/src/views/evaluation_matrix.xml');
 
-	$('.oe_table-comparison-factors').on('click', '.show-children', function (ev) {
-            ev.preventDefault();
-            var $elem = $(ev.currentTarget);
-            var comparison_factor_id = $elem.attr('comparison-factor');
-            var children = $elem.closest("tr").attr('children'); // children are displayed or hidden (hidden by default)
-            if(children == "hidden") {
-                var products = [];
-                $('.oe_comparison-product').each(function() {
-                    products.push(parseInt($(this).attr("product-id")));
-                });
-                openerp.jsonRpc('/comparison/load_children', 'call', {
-                    'comparison_factor_id': +comparison_factor_id,
-                    'comparison_products': products,
-                }).then(function (res) {
-                    $('.row_' + comparison_factor_id).after(qweb.render("comparison_factor_children", {'comp_factor_children': res.comp_factor_children, 'comparison_results' : res.comparison_results, 'comparison_products' : res.comparison_products}));
-                    $('.row_' + comparison_factor_id).attr('children','displayed');
-                });
-            }
-            else {
-                $('.child_row_' + comparison_factor_id).remove();
-                $('.row_' + comparison_factor_id).closest("tr").attr('children','hidden');
-            }
-    });
-
+	$('.oe_table-comparison-factors')
+        .on('click', '.show-children', function (ev) {
+                ev.preventDefault();
+                var $elem = $(ev.currentTarget);
+                var comparison_factor_id = $elem.attr('comparison-factor');
+                var children = $elem.closest("tr").attr('children'); // children are displayed or hidden (hidden by default)
+                if(children == "hidden") {
+                    var products = [];
+                    $('.oe_comparison-product').each(function() {
+                        products.push(parseInt($(this).attr("product-id")));
+                    });
+                    openerp.jsonRpc('/comparison/load_children', 'call', {
+                        'comparison_factor_id': +comparison_factor_id,
+                        'comparison_products': products,
+                    }).then(function (res) {
+                        $('.row_' + comparison_factor_id).after(qweb.render("comparison_factor_children", {'comp_factor_children': res.comp_factor_children, 'comparison_results' : res.comparison_results, 'comparison_products' : res.comparison_products}));
+                        $('.row_' + comparison_factor_id).attr('children','displayed');
+                    });
+                }
+                else {
+                    $('.child_row_' + comparison_factor_id).remove();
+                    $('.row_' + comparison_factor_id).closest("tr").attr('children','hidden');
+                }
+        });
 });
