b3d3a9c98ef524cd996ba62c5a42757108290b48
jfo <jfo@odoo.com>
2014-10-17 11:34:01 +0200

    Temporary access rights added

--- addons/evaluation_matrix/__openerp__.py
+++ addons/evaluation_matrix/__openerp__.py
@@ -29,7 +29,8 @@
         'data/comparison_item.csv',
         'data/comparison_factor_result.csv',
         'data/comparison_vote_values.csv',
-        'data/comparison_vote.csv'
+        'data/comparison_vote.csv',
+        'security/ir.model.access.csv',
     ],
     'qweb': [
         'static/src/views/*.xml',
