0a881ed5e8fe2ed1101d857699f025252b3e064a
mak <mak@openerp.com>
2014-10-17 13:00:32 +0530

    [IMP] improve javascript side code.

--- addons/website_twitter_wall/static/src/js/website_twitter_wall.editor.js
+++ addons/website_twitter_wall/static/src/js/website_twitter_wall.editor.js
@@ -3,6 +3,7 @@
     var website = openerp.website;
     var wall_name = '';
     var wall_description = '';
+    var image = '';
     website.add_template_file('/website_twitter_wall/static/src/xml/website_twitter_wall_editor.xml');
     website.EditorBarContent.include({
         new_twitter_wall: function () {
@@ -27,72 +28,74 @@
         start: function () {
             var self = this;
             var $modal = self.$el;
-            // $("#myTags").tagit();
             $modal.modal();
-            $("#h_ele").val("");
-            //$('#image').attr('src','/website_twitter_wall/static/src/img/document.png');
         },
         image_upload: function(e){
             var self = this;
-            $("#h_ele").val("");
             self.error("");
-            $("div.error-dialog").remove();
-            $('input[name="url"]').val("");
+            image = '';
+            this.$("div.error-dialog").remove();
+            this.$('input[name="url"]').val("");
+            this.$('#image').attr('src','/website_twitter_wall/static/src/img/document.png');
             var fileName = e.target.files[0];
             var fr = new FileReader();
             fr.onload = function(ev){
                 $('.show_image img').remove();
                 $('.show_image').html("<span class='oe-image-thumbnail slide-img-border' style='max-height: 168px;'><img src='"+ev.target.result+"' id='image' class='img-responsive' title='Preview' style='width: 100%;'/></span>");
-                //$("#image").removeAttr("src").attr("src",ev.target.result)
-                $("#h_ele").val(ev.target.result.split(',')[1]);
+                image = ev.target.result.split(',')[1]
+                console.log(image);
             }
             fr.readAsDataURL(fileName);
         },
         image_url:function(e){
             var self = this;
+            image = '';
+            this.$('.url-error').hide();
             var testRegex = /^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/;
-            $("#h_ele").val("");
-            $("#image_upload").val("");
+            this.$("#image_upload").val("");
+            this.$('#image').attr('src','/website_twitter_wall/static/src/img/document.png');
             self.error("");
-            $("div.error-dialog").remove();
+            this.$("div.error-dialog").remove();
             var url = e.target.value;
             if (testRegex.test(url)){
-                $('.show_image img').remove();
-                $('.show_image').html("<span class='oe-image-thumbnail slide-img-border' style='max-height: 168px;'><img src='"+url+"' id='image' class='img-responsive' title='Preview' style='width: 100%;'/></span>");
-                $("#h_ele").val(url);                 
+                this.$('.show_image img').remove();
+                this.$('.show_image').html("<span class='oe-image-thumbnail slide-img-border' style='max-height: 168px;'><img src='"+url+"' id='image' class='img-responsive' title='Preview' style='width: 100%;'/></span>");
+                image = url;
             }else{
-                self.error("");
-                self.error("Enter valid image URL.");
+                this.$('.url-error').show();
+                this.$('#image_url').focus();
                 e.target.value = "";
                 return;
             }            
         },
         text_wallname:function(e){
             var self = this;
+            wall_name = '';
             if (e.target.value.length > 0){
                 self.error("");
                 wall_name = e.target.value;
-                $("div.error-dialog").remove();    
+                this.$("div.error-dialog").remove();    
             }else{
+                self.error("Must Enter Wall Name");
                 return false;
             }
             
         },
         text_description:function(e){
             var self = this;
+            wall_description = '';
             if (e.target.value.length > 0){
                 self.error("");
                 wall_description = e.target.value;
-                $("div.error-dialog").remove();    
+                this.$("div.error-dialog").remove();    
             }else{
+                self.error("Enter description");
                 return false;
             }
         },
         save: function () {
             console.log("SAve" + wall_name);
             var self = this;
-            var image = $("#h_ele").val();
-            var include_retweet = ($('#include_retweet').attr('checked'))?'TRUE':'FALSE';
             if(!image){
                 self.error("");
                 self.error("Upload Image.");
@@ -117,7 +120,6 @@
                 data:{
                     'wall_name':wall_name,
                     'image':image,
-                    'include_retweet':include_retweet,
                     'wall_description':wall_description
                 },
                 success: function(data) {
