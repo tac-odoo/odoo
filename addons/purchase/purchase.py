bf4c9e6181a09d9cf61d9d06dc2745a58ef813b0
Josse Colpaert <jco@odoo.com>
2014-10-24 11:56:48 +0200

    [IMP] Group Creation of PO Lines

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -31,6 +31,7 @@ import openerp.addons.decimal_precision as dp
 from openerp.osv.orm import browse_record_list, browse_record, browse_null
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP
 from openerp.tools.float_utils import float_compare
+from profilehooks import profile
 
 class purchase_order(osv.osv):
 
@@ -1279,6 +1280,7 @@ class procurement_order(osv.osv):
         ''' returns the main supplier of the procurement's product given as argument'''
         return procurement.product_id.seller_id
 
+    #@profile(immediate=True)
     def _get_po_line_values_from_proc(self, cr, uid, procurement, partner, company, schedule_date, context=None):
         if context is None:
             context = {}
@@ -1315,6 +1317,15 @@ class procurement_order(osv.osv):
             'taxes_id': [(6, 0, taxes)],
         }
 
+
+
+
+
+
+
+
+
+    @profile(immediate=True)
     def make_po(self, cr, uid, ids, context=None):
         """ Resolve the purchase from procurement, which may result in a new PO creation, a new PO line creation or a quantity change on existing PO line.
         Note that some operations (as the PO creation) are made as SUPERUSER because the current user may not have rights to do it (mto product launched by a sale for example)
