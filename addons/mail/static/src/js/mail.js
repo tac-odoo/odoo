tac-odoo/odoo

commit e6790ba87d8a945062f1f51eb674ae3e1958fb39

[FIX] mail: position of show more message button

addons/mail/static/src/js/mail.js
@@ -880,7 +880,7 @@ openerp.mail = function (session) {
             self.parent_thread.message_fetch(this.domain, this.context, false, function (arg, data) {
                 self.id = false;
                 // insert the message on dom after this message
-                self.parent_thread.switch_new_message( data, self.$el );
+                self.parent_thread.switch_new_message( data, self.$el.parent() );
                 self.animated_destroy(200);
             });
 

commit bc681e40ce89d1616b60e4831ce2cde7de03391a

[FIX] mail: append messages at the right place
e6790ba introduced a bug where messages were always appended at the end
of the thread, potentially displaying them in the wrong order.

addons/mail/static/src/js/mail.js
@@ -880,7 +880,7 @@ openerp.mail = function (session) {
             self.parent_thread.message_fetch(this.domain, this.context, false, function (arg, data) {
                 self.id = false;
                 // insert the message on dom after this message
-                self.parent_thread.switch_new_message( data, self.$el.parent() );
+                self.parent_thread.switch_new_message(data, self.$el);
                 self.animated_destroy(200);
             });
 