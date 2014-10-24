7c91a3ad307bb49b889f1e92bd8fb8fb546dba5d
Thibault Francois <tfr@openerp.com>
2014-10-24 17:08:26 +0200

    [FIX] Inheritance mechanism, keep only one view per key, if the key is define, and prefer the one with the right website. Always remove view with the wrong website

--- addons/website_multi/models/ir_ui_view.py
+++ addons/website_multi/models/ir_ui_view.py
@@ -2,6 +2,7 @@ from lxml import etree
 
 from openerp import tools
 from openerp.osv import osv, fields, orm
+from openerp import SUPERUSER_ID
 
 
 class view(osv.osv):
@@ -71,14 +72,30 @@ class view(osv.osv):
         
     def get_inheriting_views_arch(self, cr, uid, view_id, model, context=None):
         arch = super(view, self).get_inheriting_views_arch(cr, uid, view_id, model, context=context)
-        view_arch = dict([(v, a) for a, v in arch])
-        if context and 'website_id' in context:
-            for view_rec in self.browse(cr, 1, view_arch.keys(), context):
-                if view_rec.website_id and view_rec.website_id.id != context['website_id']:
-                    view_arch.pop(view_rec.id)
-                    
-        res = [(view_arch[view_id], view_id) for _, view_id  in arch if view_arch.get(view_id)]
-        return res
+        if not context or not 'website_id' in context:
+            return arch
+        
+        view_ids = [v for _, v in arch]
+        view_arch_to_add_per_key = {}
+        keep_view_ids = []
+        for view_rec in self.browse(cr, SUPERUSER_ID, view_ids, context):
+            #case 1: there is no key, always keep the view
+            if not view_rec.key:
+                keep_view_ids.append(view_rec.id)
+                
+            #case 2: Correct website
+            elif view_rec.website_id and view_rec.website_id.id == context['website_id']:
+                view_arch_to_add_per_key[view_rec.key] = (view_rec.website_id.id, view_rec.id)
+            #case 3: no website add it if no website
+            if not view_rec.website_id:
+                view_web_id, view_id = view_arch_to_add_per_key.get(view_rec.key, (False, False))
+                if not view_web_id:
+                    view_arch_to_add_per_key[view_rec.key] = (False, view_rec.id)
+                #else: do nothing, you already have the right view
+            #case 4: website is wrong: do nothing
+        #Put all the view_id we keep together
+        keep_view_ids.extend([view_id for _, view_id in view_arch_to_add_per_key.values()])
+        return [(arch, view_id) for arch, view_id  in arch if view_id in keep_view_ids]
       
       
 
