37ebcfd7e1484cacbff9632801547345b7405401
Denis Ledoux <dle@odoo.com>
2014-11-04 11:27:25 +0100

    [FIX] product: standard_price is a property and therefore company dependent

--- addons/product/product.py
+++ addons/product/product.py
@@ -590,7 +590,9 @@ class product_template(osv.osv):
             if ptype != 'standard_price':
                 res[product.id] = product[ptype] or 0.0
             else:
-                res[product.id] = product.sudo()[ptype]
+                user = self.pool['res.users'].read(cr, uid, uid, ['company_id'], context=context)
+                product = product.with_context(force_company=user['company_id'][0])
+                res[product.id] = res[product.id] = product.sudo()[ptype]
             if ptype == 'list_price':
                 res[product.id] += product._name == "product.product" and product.price_extra or 0.0
             if 'uom' in context:
