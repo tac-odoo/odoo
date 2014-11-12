0396a0403a3c604a0c3d16ff6b84d08546405be1
pvy <pvy@odoo.com>
2014-11-12 15:10:33 +0100

    [WIP] account : financial reports

--- addons/account/account_financial_report.py
+++ addons/account/account_financial_report.py
@@ -22,9 +22,10 @@
 from openerp import models, fields, api, exceptions
 from openerp.tools.safe_eval import safe_eval
 import re
+import time
 
 
-class account_financial_report(models.Model):
+class report_account_financial_report(models.Model):
     _name = "account.financial.report"
     _description = "Account Report"
 
@@ -44,6 +45,19 @@ class account_financial_report(models.Model):
     def _check_columns(self):
         if not (self.debit_credit or self.balance or self.comparison):
             raise exceptions.ValidationError("Report needs at least one column")
+
+    @api.multi
+    def render_html(self, data=None):
+        report_obj = self.env['report']
+        report = report_obj._get_report_from_name('account.report_financial')
+        docargs = {
+            'doc_ids': self._ids,
+            'doc_model': report.model,
+            'docs': self,
+            'get_lines': self.line.get_lines,
+            'time': time,
+        }
+        return report_obj.render('account.report_financial', docargs)
 
 
 class account_financial_report_line(models.Model):
@@ -69,29 +83,6 @@ class account_financial_report_line(models.Model):
         return res
 
     @api.one
-    @api.constrains('type', 'formula')
-    def _check_formula(self):
-        formula = self.formula
-        if self.type == 'formula':
-            blocks = re.findall('\[.*\]', formula)
-            for block in blocks:
-                split_block = block.split(',')
-                if len(split_block) < 1 or len(split_block) > 2:
-                    raise exceptions.ValidationError(block + "doesn't have 1 or 2 arguments")
-                else:
-                    if self.search_count([('code', '=', split_block[0])]) < 1:
-                        raise exceptions.ValidationError("First argument must be a report code in " + block)
-                    if len(split_block) == 2 and split_block[1] not in ['balance', 'credit', 'debit', 'comparison']:
-                        raise exceptions.ValidationError("Second argument must be a column name in " + block)
-                formula.replace(block, '1')
-            formula.replace(' #Days ', ' 1 ')
-            formula.replace(' #Months ', ' 1 ')
-            try:
-                int(safe_eval(formula))
-            except Exception:
-                raise exceptions.ValidationError("Invalid formula")
-
-    @api.one
     def _get_balance(self, field_names, args):
         '''returns the balance (or other fields) computed for this record. If the record is of type :
                'accounts' : it's the sum of the linked accounts
@@ -115,19 +106,40 @@ class account_financial_report_line(models.Model):
                     res[field] += value[field]
         elif self.type == 'formula':
             # a formula is used to tell what should be displayed
-            formula = self.formula
-            formula.replace(' #Days ', )
-            formula.replace(' #Month ', )
-            blocks = re.findall('\[.*\]', self.formula)
-            for block in blocks:
-                split_block = block.split(',')
-                report_code = split_block[0]
-                column = 'balance'
-                if len(split_block) == 2:
-                    column = split_block[1]
-                target_report = self.search([('code', '=', report_code)], limit=1)
-                value = target_report._get_balance(column, args)
+            for field in field_names:
+                formula = '0'
+                for form in self.formulas:
+                    if form.column == field:
+                        formula = form.formula
+                formula = self.formula
+                formula.replace('#Days', '1')
+                formula.replace('#Month', '1')
+                blocks = re.findall('\[.*\]', self.formula)
+                for block in blocks:
+                    split_block = block.split(',')
+                    report_code = split_block[0]
+                    column = 'balance'
+                    if len(split_block) == 2:
+                        column = split_block[1]
+                    target_report = self.search([('code', '=', report_code)], limit=1)
+                    value = target_report._get_balance(column, args)
+                    formula.replace(block, value)
+                res[field] += safe_eval(formula)
         return res
+
+    @api.one
+    @api.constrains('formulas')
+    def _check_formulas(self):
+        counters = {
+            'debit': 0,
+            'credit': 0,
+            'balance': 0,
+            'Comparison': 0,
+        }
+        for formula in self.formulas:
+            counters[formula.column] += 1
+            if counters[formula.column] > 1:
+                raise exceptions.ValidationError("A report line can only have one formula per column")
 
     name = fields.Char('Line Name')
     code = fields.Char('Line Code')
@@ -137,7 +149,7 @@ class account_financial_report_line(models.Model):
         ('filter', 'Domain'),
         ('sum', 'Sum Of Children'),
         ('formula', 'Formula'),
-    ], 'Type', required=True)
+    ], 'Type', required=True, default='sum')
     balance = fields.Integer(compute='_get_balance', string='Balance', multi='balance')
     debit = fields.Integer(compute='_get_balance', string='Debit', multi='balance')
     credit = fields.Integer(compute='_get_balance', string='Credit', multi="balance")
@@ -154,7 +166,6 @@ class account_financial_report_line(models.Model):
         ('moves', 'All Journal Items'),
         ('no_detail', 'No Detail'),
     ], 'Display Details')
-    formula = fields.Char('Formula')
     parent_id = fields.Many2one('account.financial.report.line', 'Parent')
     children_ids = fields.One2many('account.financial.report.line', 'parent_id', 'Children')
     financial_report = fields.Many2one('account.financial.report', 'Financial Report')
@@ -165,24 +176,24 @@ class account_financial_report_line(models.Model):
     sign = fields.Selection([
         (-1, 'Reverse balance sign'),
         (1, 'Preserve balance sign')
-    ], 'Sign on Reports', required=True,
+    ], 'Sign on Reports', required=True, default=1,
        help="For accounts that are typically more debited than credited and that you would like to "
             "print as negative amounts in your reports, you should reverse the sign of the balance; e.g.: "
             "Expense account. The same applies for accounts that are typically more credited than debited "
             "and that you would like to print as positive amounts in your reports; e.g.: Income account.")
     ir_filter_id = fields.Many2one('ir.filters', 'Domain', ondelete='cascade')
     group_by = fields.Char('Group By')
+    formulas = fields.Many2many('account.financial.report.line.formula', 'account_formula_line', 'line_id', 'formula_id', 'Formulas')
 
     def get_lines(self, data):
         lines = []
         account_obj = self.env['account.account']
-        currency_obj = self.env['res.currency']
         for line in self.with_context(data['form']['used_context'])._get_children_by_order():
             vals = {
                 'name': line.name,
                 'type': 'line',
                 'level': line.level,
-                'account_type': line.type == 'sum' and 'view' or False,#used to underline the financial line balances
+                'account_type': line.type == 'sum' and 'view' or False,  # used to underline the financial line balances
             }
             if self.financial_report.balance:
                 vals['balance'] = line.balance * line.sign or 0.0
@@ -210,18 +221,53 @@ class account_financial_report_line(models.Model):
                         'name': account.code + ' ' + account.name,
                         'balance':  account.balance != 0 and account.balance * line.sign or account.balance,
                         'type': 'account',
-                        'level': line.display_detail == 'detail_with_hierarchy' and min(account.level + 1, 6) or 6,#account.level + 1
+                        'level': line.display_detail == 'detail_with_hierarchy' and min(account.level + 1, 6) or 6,  # account.level + 1
                         'account_type': account.type,
                     }
                     if self.financial_report.debit_credit:
                         vals['debit'] = account.debit
                         vals['credit'] = account.credit
-                    if not currency_obj.is_zero(account.company_id.currency_id, vals['balance']):
+                    if not account.company_id.currency_id.is_zero(vals['balance']):
                         flag = True
                     if self.financial_report.comparison:
-                        vals['balance_cmp'] = account_obj.with_context(data['form']['comparison_context']).browse(account.id).balance * line.sign or 0.0
-                        if not currency_obj.is_zero(account.company_id.currency_id, vals['balance_cmp']):
+                        account_id = account_obj.with_context(data['form']['comparison_context']).browse(account.id)
+                        vals['balance_cmp'] = account_id.balance * line.sign or 0.0
+                        if not account.company_id.currency_id.is_zero(vals['balance_cmp']):
                             flag = True
                     if flag:
                         lines.append(vals)
         return line
+
+
+class account_financial_report_line_formula(models.Model):
+    _name = "account.financial.report.line.formula"
+    _description = "Matching formulas with columns"
+
+    @api.one
+    @api.constrains('formula')
+    def _check_formula(self):
+        formula = self.formula
+        if self.type == 'formula':
+            blocks = re.findall('\[.*\]', formula)
+            for block in blocks:
+                split_block = block.split(',')
+                if len(split_block) < 1 or len(split_block) > 2:
+                    raise exceptions.ValidationError(block + " must have 1 or 2 arguments")
+                else:
+                    report_line_obj = self.env['account.financial.report.line']
+                    if report_line_obj.search_count([('code', '=', split_block[0])]) < 1:
+                        raise exceptions.ValidationError("First argument must be a report code in " + block)
+                    if len(split_block) == 2 and split_block[1] not in ['balance', 'credit', 'debit', 'comparison']:
+                        raise exceptions.ValidationError("Second argument must be a column name in " + block)
+                formula.replace(block, '1')
+            formula.replace('#Days', '1')
+            formula.replace('#Months', '1')
+            try:
+                int(safe_eval(formula))
+            except Exception:
+                raise exceptions.ValidationError("Invalid formula")
+
+    column = fields.Selection([('balance', 'Balance'), ('credit', 'Credit'),
+                               ('debit', 'Debit'), ('comparison', 'Comparison')],
+                              'Column Name')
+    formula = fields.Char('Formula')
