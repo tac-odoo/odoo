8c7e94a2788a176ee2d464841dd401200137c5bf
Damien Bouvy <dbo@odoo.com>
2014-11-17 17:21:28 +0100

    Migrating marketing_campaign to lead_automation (new API)

---
+++ addons/lead_automation/models.py
@@ -0,0 +1,276 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, exceptions
+import time
+
+import pdb
+
+class lead_automation_campaign(models.Model):
+	_name = "lead.automation.campaign"
+
+
+	name= fields.Char('Name', required=True)
+	object_id= fields.Many2one('ir.model', 'Resource', required=True,
+																help="Choose the resource on which you want this campaign to be run")
+	partner_field_id= fields.Many2one('ir.model.fields', 'Partner Field',
+																					domain="[('model_id', '=', object_id), ('ttype', '=', 'many2one'), ('relation', '=', 'res.partner')]",
+																					help="The generated workitems will be linked to the partner related to the record. "\
+																					"If the record is the partner itself leave this field empty. "\
+																					"This is useful for reporting purposes, via the Campaign Analysis or Campaign Follow-up views.")
+	unique_field_id= fields.Many2one('ir.model.fields', 'Unique Field',
+																					domain="[('model_id', '=', object_id), ('ttype', 'in', ['char','int','many2one','text','selection'])]",
+																					help='If set, this field will help segments that work in "no duplicates" mode to avoid '\
+																					'selecting similar records twice. Similar records are records that have the same value for '\
+																					'this unique field. For example by choosing the "email_from" field for CRM Leads you would prevent '\
+																					'sending the same campaign to the same email address again. If not set, the "no duplicates" segments '\
+																					"will only avoid selecting the same record again if it entered the campaign previously. "\
+																					"Only easily comparable fields like textfields, integers, selections or single relationships may be used.")
+	mode=fields.Selection([('test', 'Test Directly'),
+																		('test_realtime', 'Test in Realtime'),
+																		('manual', 'With Manual Confirmation'),
+																		('active', 'Normal')],default='test',
+																	string='Mode', required=True, help= """Test - It creates and process all the activities directly (without waiting for the delay on transitions) but does not send emails or produce reports.
+																																	Test in Realtime - It creates and processes all the activities directly but does not send emails or produce reports.
+																																	With Manual Confirmation - the campaigns runs normally, but the user has to validate all workitem manually.
+																																	Normal - the campaign runs normally and automatically sends all emails and reports (be very careful with this mode, you're live!)""")
+	state= fields.Selection([('draft', 'New'),
+                                   ('running', 'Running'),
+                                   ('cancelled', 'Cancelled'),
+                                   ('done', 'Done')],
+                                   'Status', copy=False, default='draft')
+ 	activity_ids= fields.One2many('lead.automation.activity', 'campaign_id', 'Activities')
+ 	fixed_cost= fields.Float('Fixed Cost', help="Fixed cost for running this campaign. You may also specify variable cost and revenue on each campaign activity. Cost and Revenue statistics are included in Campaign Reporting.")
+ 	segment_ids= fields.One2many('lead.automation.segment', 'campaign_id', 'Segments', readonly=False)
+ 	segments_count= fields.Integer(compute='_count_segments', string='Segments')
+
+	@api.one
+	def _count_segments(self):
+		self.segments_count = len(self.segment_ids)
+
+	@api.one
+	def action_draft(self):
+		self.state = 'draft'
+
+	@api.one
+	def action_run(self):
+		if not self.activity_ids:
+			raise exceptions.ValidationError("A Campaign without any Activity cannot be set to Run.")
+
+		has_start = False
+		has_signal_without_from = False
+		for activity in self.activity_ids:
+			if activity.start:
+				has_start = True
+			if activity.signal and len(activity.from_ids) == 0:
+				has_signal_without_from = True
+
+		if not has_start and not has_signal_without_from:
+			raise exceptions.ValidationError("The campaign cannot be started. It does not have any starting activity. Modify campaign's activities to mark one as the starting point.")
+
+		self.state = 'running'
+
+	@api.one
+	def action_done(self):
+		has_active_segment = False
+		for segment in self.segment_ids:
+			if segment.state == 'running':
+				has_active_segment = True
+
+		if has_active_segment:
+			raise exceptions.ValidationError('The campaign cannot be marked as done before all segments are closed.')
+		self.state = 'done'
+
+	@api.one
+	def action_cancel(self):
+		self.state = 'cancelled'
+
+	# prevent duplication until the server properly duplicates several levels of nested o2m
+	@api.one
+	def copy(self, default=None):
+		raise exceptions.ValidationError("You cannot duplicate a campaign, Not supported yet.")
+
+
+class lead_automation_segment(models.Model):
+	_name="lead.automation.segment"
+
+
+	name= fields.Char('Name', required=True)
+	campaign_id= fields.Many2one('lead.automation.campaign', 'Campaign', required=True, select=1, ondelete="cascade")
+	object_id= fields.Many2one(related='campaign_id.object_id', string='Resource',store=True)
+	ir_filter_id= fields.Many2one('ir.filters', 'Filter', ondelete="restrict",
+																		help="Filter to select the matching resource records that belong to this segment. "\
+																		"New filters can be created and saved using the advanced search on the list view of the Resource. "\
+																		"If no filter is set, all records are selected without filtering. "\
+																		"The synchronization mode may also add a criterion to the filter.")
+	sync_last_date= fields.Datetime('Last Synchronization', help="Date on which this segment was synchronized last time (automatically or manually)"),
+	sync_mode= fields.Selection([('create_date', 'Only records created after last sync'),
+																('write_date', 'Only records modified after last sync (no duplicates)'),
+																('all', 'All records (no duplicates)')],
+																string='Synchronization mode',
+																default='create_date',
+																help="Determines an additional criterion to add to the filter when selecting new records to inject in the campaign. "\
+																'"No duplicates" prevents selecting records which have already entered the campaign previously.'\
+																'If the campaign has a "unique field" set, "no duplicates" will also prevent selecting records which have '\
+																'the same value for the unique field as other records that already entered the campaign.')
+	state= fields.Selection([('draft', 'New'),
+															('cancelled', 'Cancelled'),
+															('running', 'Running'),
+															('done', 'Done')],
+															string='Status',default="draft", copy=False)
+	date_run= fields.Datetime('Launch Date', help="Initial start date of this segment.")
+	date_done= fields.Datetime('End Date', help="Date this segment was last closed or cancelled.")
+	date_next_sync= fields.Datetime(compute='_get_next_sync', string='Next Synchronization')
+	sync_last_date = fields.Datetime('Last Synchronization', help="Date on which this segment was synchronized last time (automatically or manually)")
+
+	def _get_next_sync(self):
+		segment_cron=self.env['ir.cron'].search([('model','=','lead.automation.segment')])
+		self.date_next_sync= fields.Datetime.from_string(segment_cron.nextcall)
+
+	@api.constrains('ir_filter_id','campaign_id')
+	@api.one
+	def check_object_model(self):
+		if self.object_id.model!=self.ir_filter_id.model_id:
+			raise exceptions.ValidationError("Model of filter must be same as resource model of Campaign.")
+
+	@api.one
+	def action_draft(self):
+		self.state = 'draft'
+
+	@api.one
+	def action_run(self):
+		if not(self.date_run):
+			self.date_run = time.strftime('%Y-%m-%d %H:%M:%S')
+		self.state = 'running'
+
+	@api.one
+	def action_done(self):
+		# TODO: cancel all workitems related to that segment
+		self.state = 'done'
+
+	@api.one
+	def action_cancel(self):
+		# TODO: cancel all workitems related to that segment
+		self.state = 'cancelled'
+
+
+class lead_automation_activity(models.Model):
+	_name='lead.automation.activity'
+	_action_types = [
+		('email', 'Email'),
+		('report', 'Report'),
+		('action', 'Custom Action'),
+		# TODO implement the subcampaigns.
+		# TODO implement the subcampaign out. disallow out transitions from
+		# subcampaign activities ?
+		#('subcampaign', 'Sub-Campaign'),
+	]
+	name= fields.Char('Name', required=True)
+	campaign_id= fields.Many2one('lead.automation.campaign', 'Campaign',
+                                            required = True, ondelete='cascade', select=1)
+	object_id= fields.Many2one(related='campaign_id.object_id', string='Object', readonly=True)
+	start= fields.Boolean('Start', help= "This activity is launched when the campaign starts.", select=True)
+	condition= fields.Text('Condition', size=256, required=True, default="True",
+                                 help="Python expression to decide whether the activity can be executed, otherwise it will be deleted or cancelled."
+                                 "The expression may use the following [browsable] variables:\n"
+                                 "   - activity: the campaign activity\n"
+                                 "   - workitem: the campaign workitem\n"
+                                 "   - resource: the resource object this campaign item represents\n"
+                                 "   - transitions: list of campaign transitions outgoing from this activity\n"
+                                 "...- re: Python regular expression module")
+	type= fields.Selection(_action_types, 'Type', required=True, default="email",
+                                  help="""The type of action to execute when an item enters this activity, such as:
+   - Email: send an email using a predefined email template
+   - Report: print an existing Report defined on the resource item and save it into a specific directory
+   - Custom Action: execute a predefined action, e.g. to modify the fields of the resource record
+  """)
+	email_template_id= fields.Many2one('email.template', "Email Template", help='The email to send when this activity is activated')
+	report_id= fields.Many2one('ir.actions.report.xml', "Report", help='The report to generate when this activity is activated', )
+	report_directory_id= fields.Many2one('document.directory','Directory',
+                                help="This folder is used to store the generated reports")
+	server_action_id= fields.Many2one('ir.actions.server', string='Action',
+                                help= "The action to perform when this activity is activated")
+	to_ids= fields.One2many('lead.automation.transition',
+                                           'activity_from_id',
+                                           'Next Activities')
+	from_ids= fields.One2many('lead.automation.transition',
+                                           'activity_to_id',
+                                           'Previous Activities')
+	#variable_cost: fields.float('Variable Cost', help="Set a variable cost if you consider that every campaign item that has reached this point has entailed a certain cost. You can get cost statistics in the Reporting section", digits_compute=dp.get_precision('Product Price'))
+	#revenue: fields.float('Revenue', help="Set an expected revenue if you consider that every campaign item that has reached this point has generated a certain revenue. You can get revenue statistics in the Reporting section", digits_compute=dp.get_precision('Account'))
+	signal= fields.Char('Signal',
+                              help='An activity with a signal can be called programmatically. Be careful, the workitem is always created when a signal is sent')
+	keep_if_condition_not_met= fields.Boolean("Don't Delete Workitems",help="By activating this option, workitems that aren't executed because the condition is not met are marked as cancelled instead of being deleted.")
+
+	@api.one
+	def process():
+		return True
+
+
+class lead_automation_transition(models.Model):
+	_name = 'lead.automation.transition'
+
+	_interval_units = [
+        ('hours', 'Hour(s)'),
+        ('days', 'Day(s)'),
+        ('months', 'Month(s)'),
+        ('years', 'Year(s)'),
+    ]
+
+	name= fields.Char(compute='_get_name', string='Name', size=128)
+	activity_from_id= fields.Many2one('lead.automation.activity','Previous Activity', select=1,required=True, ondelete="cascade")
+	activity_to_id= fields.Many2one('lead.automation.activity','Next Activity',required=True, ondelete="cascade")
+	interval_nbr= fields.Integer('Interval Value', required=True, default=1)
+	interval_type= fields.Selection(_interval_units, 'Interval Unit',required=True,default="days")
+
+	trigger= fields.Selection([('auto', 'Automatic'),
+																			('time', 'Time'),
+																			('cosmetic', 'Cosmetic'),  # fake plastic transition
+																			],
+																			'Trigger', required=True, default='time',
+																			help="How is the destination workitem triggered")
+	@api.one
+	def _get_name(self):
+		# name formatters that depend on trigger
+		formatters = {
+			'auto': _('Automatic transition'),
+			'time': _('After %(self.interval_nbr)d %(self.interval_type)s'),
+			'cosmetic': _('Cosmetic'),
+		}
+		self.name = formatters[self.trigger]
+
+
+class lead_automation_workitem(models.Model):
+	_name = "lead.automation.workitem"
+
+	segment_id= fields.Many2one('lead.automation.segment', 'Segment', readonly=True)
+	activity_id= fields.Many2one('lead.automation.activity','Activity',required=True, readonly=True)
+	campaign_id= fields.Many2one(related='activity_id.campaign_id', string='Campaign', readonly=True, store=True)
+	object_id= fields.Many2one(related='activity_id.campaign_id.object_id', string='Resource', select=1, readonly=True, store=True)
+	res_id= fields.Integer('Resource ID', select=1, readonly=True)
+	res_name= fields.Char(compute='_res_name_get', string='Resource Name', search='_resource_search', size=64)
+	date= fields.Datetime('Execution Date', default=False, help='If date is not set, this workitem has to be run manually', readonly=True)
+	partner_id= fields.Many2one('res.partner', 'Partner', select=1, readonly=True)
+	state= fields.Selection([ ('todo', 'To Do'),
+															('cancelled', 'Cancelled'),
+															('exception', 'Exception'),
+															('done', 'Done'),
+														], 'Status', default='todo', readonly=True, copy=False)
+	error_msg= fields.Text('Error Message', readonly=True)
+
+	@api.one
+	def _res_name_get(self):
+		self.res_name = self.res_id.model
+
+	@api.one
+	def _resource_search(self):
+		self.res_name = self.res_id.model
+
+	@api.one
+	def action_draft(self):
+		if self.state in  ['exception','cancelled']:
+				self.state='todo'
+
+	@api.one
+	def action_cancel(self):
+		if slef.state in ['exception','todo']:
+			self.state=cancelled
