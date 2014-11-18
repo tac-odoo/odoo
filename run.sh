e15b5fb92426f650ff9dc38cda43dc78f44760ca
Antony Lesuisse <lesuisse@gmail.com>
2014-11-18 23:09:12 +0100

    m

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#./odoo.py -r odoo -w odoo -d odooblog --db-filter=odooblog
-./odoo.py --addons-path=addons,../design-themes -r odoo -w odoo -d themes --db-filter=blog
-#./cygdrive/c/Python27/python.exe ./odoo.py --addons-path=addons,../design-themes -r odoo -w odoo -d themes --db-filter=blog
