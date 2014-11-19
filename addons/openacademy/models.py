9c53cf6aef6d6e7db594e6972a28ddb197ab0aff
Martin Trigaux <mat@odoo.com>
2014-11-19 21:59:07 +0100

    Computed fields

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -26,3 +26,13 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.one
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0.0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
