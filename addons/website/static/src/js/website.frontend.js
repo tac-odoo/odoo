e1d20baf4d3a9bc3d1a9da056671aad7cc09db0d
Ajay Patel <apa@openerp.com>
2014-11-24 11:33:53 +0530

    [IMP]website:add one selector to handle post call method to share content, and add dialog templete.

--- addons/website/static/src/js/website.frontend.js
+++ addons/website/static/src/js/website.frontend.js
@@ -9,17 +9,17 @@
         /*
             element:Element to bind popover
             social_list: List of social media avialable
-            configure: To configure socail pluging (url, width, height)
+            config: To config social plugin (width, height)
         */
-        template: 'website.social_share',
-        init: function(element, social_list, configure){
+        // template: 'website.social_share',
+        init: function(template, element, social_list, options){
             //Initialization
             this._super.apply(this, arguments);
             var self = this;
+            this.template = template || 'website.social_share';
             this.element = element;
             this.social_list = social_list;
-            this.target = 'social_share';
-            // this.element.data = {'toggle' : 'popover', 'container' : 'body', 'placement' : 'bottom', 'target' : '#' + this.target};
+            this.options = options
             this.renderElement();
             this.bind_events();
         },
@@ -28,58 +28,48 @@
         // },
         bind_events: function() {
             var self = this;
-            $('.fa-facebook').on('click', self.proxy('facebook'));
-            $('.fa-twitter').on('click', self.proxy('twitter'));
-            $('.fa-linkedin').on('click', self.proxy('linkedin'));
-            $('.fa-google-plus').on('click', self.proxy('google_plus'));
+            $('.facebook').on('click', $.proxy(self.renderSocial, self, 'facebook'));
+            $('.twitter').on('click', $.proxy(self.renderSocial, self, 'twitter'));
+            $('.linkedin').on('click', $.proxy(self.renderSocial, self, 'linkedin'));
+            $('.google-plus').on('click', $.proxy(self.renderSocial, self, 'google-plus'));
         },
         renderElement: function() {
             var self = this;
-            this.$el.append(openerp.qweb.render(this.template, {medias: this.social_list, id: this.target}));
-            console.log("calll renderElement",this.element, this.$el);
-            
-            self.element.popover({
-                'content': self.$el.html(),
-                'placement': 'bottom',
-                'container': 'body',
-                'html': true,
-                'trigger': 'manual',
-                'animation': false,
-            }).popover("show")
-            .on("mouseleave", function () {
-                var _this = this;
-                setTimeout(function () {
-                    if (!$(".popover:hover").length) {
-                        $(_this).popover("hide")
-                    }
-                }, 100);
-            });
-            $('.popover').on("mouseleave", function () {
-                $(this).hide();
-            });
-        },
-        google_plus: function(){
-            //call schema template
-            console.log("call google-plus");
-            this.renderSocial('google-plus');
-        },
-        facebook: function(){
-            // call opengraphtemplate
-            console.log("call facebook");
-            this.renderSocial('facebook');
-        },
-        twitter: function(){
-            console.log("call twitter");
-            this.renderSocial('twitter');
-        },
-        linkedin: function(){
-            console.log("call linkedin");
-            this.renderSocial('linkedin');
+            if (this.template == 'website.social_share_dialog'){
+                openerp.qweb.render(this.template, {medias: this.social_list})
+            }
+            else{
+                this.$el.append(openerp.qweb.render(this.template, {medias: this.social_list}));
+                // this.$el.append(openerp.qweb.render(this.template, {medias: this.social_list, id: this.target}));
+                console.log("calll renderElement",this.element, this.$el);
+
+                self.element.popover({
+                    'content': self.$el.html(),
+                    'placement': 'bottom',
+                    'container': 'body',
+                    'html': true,
+                    'trigger': 'manual',
+                    'animation': false,
+                }).popover("show")
+                .on("mouseleave", function () {
+                    var _this = this;
+                    setTimeout(function () {
+                        if (!$(".popover:hover").length) {
+                            $(_this).popover("hide")
+                        }
+                    }, 100);
+                });
+                $('.popover').on("mouseleave", function () {
+                    $(this).hide();
+                });
+                // openerp.qweb.add_template('website.opengraph_tags');
+                openerp.qweb.render('website.set_meta_tags', {});
+            }
         },
         renderSocial: function(social){
             var url = this.element.data('url') || window.location.href.split(/[?#]/)[0]; // get current url without query string if not pass 
             var text_to_share = this.element.data('share_content');
-            console.log("url", url, text_to_share);
+            console.log("url", url, text_to_share, social);
             var social_network = {
                 'facebook':'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url),
                 'twitter': 'https://twitter.com/intent/tweet?original_referer=' + encodeURIComponent(url) + '&amp;text=' + encodeURIComponent(text_to_share + ' - ' + url),
@@ -87,11 +77,9 @@
                 'google-plus': 'https://plus.google.com/share?url=' + encodeURIComponent(url)
             };
             if (_.contains(_.keys(social_network), social)){
-                console.log("cllllll")
                 var window_height = 500, window_width = 500, left = (screen.width/2)-(window_width/2), top = (screen.height/2)-(window_height/2);
                 window.open(social_network[social], '', 'menubar=no, toolbar=no, resizable=yes, scrollbar=yes, height=' + window_height + ',width=' + window_width + ', top=' + top + ', left=' + left);
             }
-            //find on which social icon click and reneder one of below function
         },
     });
 
@@ -100,12 +88,54 @@
             var self = $(this);
             var default_social_list = ['facebook','twitter', 'linkedin', 'google-plus']
             var social_list = _.intersection(eval($(this).data('social')) || default_social_list, default_social_list);
+            var default_config = {'default_social': {'width':500, 'height':500}}
+            var config = _.defaults(default_config, {'default_social': { 'height':100}});
+
+            console.log(config);
             new website.social_share(
+                        'website.social_share',
                         $(this),
                         social_list,
                         {'facebook': {'width':100, 'height':100}}
                     );
             // social_share_obj.set_value('type', 'description', 'title', 'url', 'image');
         });
+        $(document.body).on('hover', '.social_share_call', function() {
+            var self = $(this);
+            var default_social_list = ['facebook','twitter', 'linkedin', 'google-plus']
+            var social_list = _.intersection(eval($(this).data('social')) || default_social_list, default_social_list);
+
+            // var url = self.data('url') || window.location.href.split(/[?#]/)[0];
+            // var text_to_share = self.data('share_content');
+            // var description =  self.data('description');
+            var dataObject = {};
+            dataObject_func('social_list', social_list);
+            dataObject_func('url', self.data('url'));
+            dataObject_func('share_content', self.data('share_content'));
+            dataObject_func('description', self.data('description'));
+            function dataObject_func(propertyName, propertyValue)
+            {
+                if(propertyValue) dataObject[propertyName] = propertyValue;
+            };
+            console.log("call social_share_call ", localStorage.getItem('social_share'));
+
+            // Put the object into storage
+            localStorage.setItem('social_share', JSON.stringify(dataObject));
+
+        });
+    });
+    $(document).ready(function() {
+        if(localStorage.getItem('social_share')){
+            // Retrieve the object from storage
+            var dataObject = JSON.parse(localStorage.getItem('social_share'));
+            new website.social_share(
+                        'website.social_share_dialog',
+                        $(this),
+                        dataObject['social_list'],
+                        {'facebook': {'width':100, 'height':100}}
+                    );
+            localStorage.removeItem('social_share');
+            // console.log('retrievedObject: ', JSON.parse(dataObject));
+        }
     });
 })();
