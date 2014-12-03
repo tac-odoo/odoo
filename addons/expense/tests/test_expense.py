f1eaf1d309b41be54120562474e70141f27e8223
ssh-odoo <ssh@openerp.com>
2014-12-03 15:19:01 +0530

    [Add,imp]:add a python test case

---
+++ addons/expense/tests/test_expense.py
@@ -0,0 +1,8 @@
+from openerp.tests.common import TransactionCase
+
+class TestExpense(TransactionCase):
+
+    def setUp(self):
+        super(TestExpense, self).setUp()
+        
+    def test_expense(self):
