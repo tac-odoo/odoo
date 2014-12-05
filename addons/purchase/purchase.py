3f57b875ff7a843037be44a3a3df190f62f44349
Josse Colpaert <jco@odoo.com>
2014-12-05 14:31:52 +0100

    [IMP] Optimize method for cancelling procurements

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -1196,6 +1196,7 @@ class procurement_order(osv.osv):
                 purchase_line_obj.action_cancel(cr, uid, [procurement.purchase_line_id.id], context=context)
         return super(procurement_order, self).propagate_cancel(cr, uid, procurement, context=context)
 
+
     def _run(self, cr, uid, procurement, context=None):
         if procurement.rule_id and procurement.rule_id.action == 'buy':
             #make a purchase order for the procurement
@@ -1294,7 +1295,7 @@ class procurement_order(osv.osv):
         seller_qty = procurement.product_id.seller_qty
         pricelist_id = partner.property_product_pricelist_purchase.id
         uom_id = procurement.product_id.uom_po_id.id
-        qty = uom_obj._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, uom_id)
+        qty = uom_obj._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, uom_id) #C'est faux si ce n'est pas une nouvelle ligne
         if seller_qty:
             qty = max(qty, seller_qty)
         price = pricelist_obj.price_get(cr, uid, [pricelist_id], procurement.product_id.id, qty, partner.id, {'uom': uom_id})[pricelist_id]
