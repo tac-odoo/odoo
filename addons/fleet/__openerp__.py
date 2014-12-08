f229b17d2dbad498410b5a7bcc624f060567f862
Siddharth Bhalgami <sha@openerp.com>
2014-12-08 11:33:50 +0530

    [IMP]Fleet_new_Api: inprove code & add store=True in function field.

--- addons/fleet/__openerp__.py
+++ addons/fleet/__openerp__.py
@@ -1,32 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
 {
-    'name' : 'Fleet Management',
-    'version' : '0.1',
-    'author' : 'OpenERP S.A.',
+    'name': 'Fleet Management',
+    'version': '0.1',
+    'author': 'Odoo S.A.',
     'sequence': 110,
     'category': 'Managing vehicles and contracts',
-    'website' : 'https://www.odoo.com/page/fleet',
-    'summary' : 'Vehicle, leasing, insurances, costs',
-    'description' : """
+    'website': 'https://www.odoo.com/page/fleet',
+    'summary': 'Vehicle, leasing, insurances, costs',
+    'description': """
 Vehicle, leasing, insurances, cost
 ==================================
 With this module, OpenERP helps you managing all your vehicles, the
@@ -43,24 +25,25 @@ Main Features
 * Show all costs associated to a vehicle or to a type of service
 * Analysis graph for costs
 """,
-    'depends' : [
+    'depends': [
         'base',
         'mail',
         'board'
     ],
-    'data' : [
+    'data': [
         'security/fleet_security.xml',
         'security/ir.model.access.csv',
         'views/fleet_view.xml',
-        'views/fleet_board_view.xml',
-        'data/fleet_cars.xml',
-        'data/fleet_data.xml',
+        'views/fleet_board_view.xml'
     ],
     'images': ['images/costs_analysis.jpeg','images/indicative_costs_analysis.jpeg','images/vehicles.jpeg','images/vehicles_contracts.jpeg','images/vehicles_fuel.jpeg','images/vehicles_odometer.jpeg','images/vehicles_services.jpeg'],
 
-    'demo': ['data/fleet_demo.xml'],
+    'demo': [
+        'data/fleet_cars.xml',
+        'data/fleet_data.xml',
+        'data/fleet_demo.xml'
+    ],
 
-    'installable' : True,
-    'application' : True,
+    'installable': True,
+    'application': True,
 }
-
