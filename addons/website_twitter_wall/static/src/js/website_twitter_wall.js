fc18d385452c4fe31eb3ba4d695ccf7e7dd9dbdd
Jigar Patel <jat@openerp.com>
2014-12-10 12:25:42 +0530

    [IMP] Improve code

--- addons/website_twitter_wall/static/src/js/website_twitter_wall.js
+++ addons/website_twitter_wall/static/src/js/website_twitter_wall.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    if(window.location.href.split("/")[3]=="twitter_wall"){
+    if(window.location.href.split("/")[3] == "twitter_wall") {
         $('#oe_main_menu_navbar').css("display","none");
-        $('header').css("display","none");
-        $('footer').css("display",'none');   
-    }    
-    if($("div[name='tweets_for_client']").length){
+        $('header').css("display", "none");
+        $('footer').css("display", "none");
+    }
+    if($("div[name='tweets_for_client']").length) {
         var twitter_wall = new openerp.website.tweet_wall($("#tweet_wall_div"), parseInt($("[wall_id]").attr("wall_id")));
         twitter_wall.start();
     }
@@ -12,51 +12,45 @@ $(document).ready(function() {
 
 openerp.qweb.add_template('/website_twitter_wall/static/src/xml/website_twitter_wall.xml');
 openerp.website.tweet_wall = openerp.Class.extend({
-    template : 'twitter_tweets',
-    init : function($el, wall_id, interval_time) {
+    template: 'twitter_tweets',
+    init: function($el, wall_id, interval_time) {
         this.$el = $el;
-        this.twitter_widget = 0;
         this.get_data_duration = interval_time || 5500;
         this.show_tweet_duation = interval_time || 5000;
         this.wall_id = wall_id;
         this.show_tweet = [];
         this.shown_tweet = [];
-        this.get_data_interval_id;
-        this.show_tweet_interval_id;
         this.last_tweet_id = 0;
     },
-    
-    start: function(){
+
+    start: function() {
         var self = this;
-        this.get_data_interval_id =  setInterval(function(){return self.get_data();}, this.get_data_duration);
-        this.show_tweet_interval_id = setInterval(function(){self.process_tweet();}, this.show_tweet_duation);
+        setInterval(function() { return self.get_data(); }, this.get_data_duration);
+        setInterval(function() { self.process_tweet(); }, this.show_tweet_duation);
     },
-    
-    get_data: function(){
+
+    get_data: function() {
         var self = this;
-        return openerp.jsonRpc("/twitter_wall/pull_tweet/"+ self.wall_id, 'call', {'last_tweet' : self.last_tweet_id}).done(function(data) {
+        return openerp.jsonRpc("/twitter_wall/pull_tweet/" + self.wall_id, 'call', {'last_tweet': self.last_tweet_id}).done(function(data) {
             if (data){
                 self.last_tweet_id = data.id;
                 self.show_tweet = self.show_tweet.concat(data);
             }
         });
     },
-    
-    process_tweet : function() {
-        var self = this;
 
-        if (this.show_tweet.length){
-            var tweet = self.show_tweet.shift();
-            self.shown_tweet.push(tweet);
-            this.animate_tweet(openerp.qweb.render("twitter_tweets", {'res' : tweet}));
+    process_tweet: function() {
+        if (this.show_tweet.length) {
+            var tweet = this.show_tweet.shift();
+            this.shown_tweet.push(tweet);
+            this.animate_tweet(openerp.qweb.render("twitter_tweets", {'res': tweet}));
         }
     },
 
-    animate_tweet:function(tweet_html){
-        //$(tweet_html).prependTo(this.$el).hide().slideDown("slow");
+    animate_tweet: function(tweet_html) {
         $(tweet_html).prependTo(this.$el);
-        setTimeout(function(){
+        setTimeout(function() {
             $('.live-tweet').slideDown("slow");
-        },1600);
+        }, 1600);
     }
 });
