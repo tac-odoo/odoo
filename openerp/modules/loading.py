cb0755aeffa538d0ca858c267f7e86df143749fe
Raphael Collet <rco@openerp.com>
2014-12-16 17:20:20 +0100

    [IMP] enable loading custom models/fields and views from module data files
    Loading views for custom models from module data files was not possible because
    custom models and fields were loaded into the registry after all modules were
    loaded.  As a consequence, the view architecture did not pass the checks.
    
    This patch takes a different approach: custom models and fields are loaded
    early on in the registry, so that views can be validated.  The trick is to take
    special care for relational custom fields: we skip them if their comodel does
    not appear in the registry.  This allows to install and upgrade modules that
    create/modify custom models, fields and views for them.

--- openerp/modules/loading.py
+++ openerp/modules/loading.py
@@ -360,12 +360,6 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
                     ['to install'], force, status, report,
                     loaded_modules, update_module)
 
-        # load custom models
-        cr.execute('select model from ir_model where state=%s', ('manual',))
-        for model in cr.dictfetchall():
-            registry['ir.model'].instanciate(cr, SUPERUSER_ID, model['model'], {})
-        registry.setup_models(cr)
-
         # STEP 4: Finish and cleanup installations
         if processed_modules:
             cr.execute("""select model,name from ir_model where id NOT IN (select distinct model_id from ir_model_access)""")
