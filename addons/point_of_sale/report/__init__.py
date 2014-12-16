0bd6f4f6be2262788261fcac10863cea2ccf7c60
Yannick Tivisse <yti@odoo.com>
2014-12-16 10:13:16 +0100

    [IMP] COD Solution

--- addons/point_of_sale/report/__init__.py
+++ addons/point_of_sale/report/__init__.py
@@ -28,5 +28,6 @@ import pos_details
 import pos_payment_report
 import pos_report
 import pos_order_report
+import so_pos_order_report
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
